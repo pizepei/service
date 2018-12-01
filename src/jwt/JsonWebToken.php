3ebe9db4d79929570ce06e085ce0f3a0aa5898f5
皮泽培 <848733913@qq.com>
2018-12-01 18:01:53 +0800

    JWT配置

--- src/jwt/JsonWebToken.php
+++ src/jwt/JsonWebToken.php
@@ -6,14 +6,16 @@
  * Time: 10:03
  * JSON Web Token基础类
  */
-namespace pizepei\service\webSocket;
+namespace pizepei\service\jwt;
+use pizepei\config\JsonWebTokenConfig;
+
 class JsonWebToken
 {
 
     /**
      *jwtPayload
      */
-    const jwtPayload = [
+    const Payload = [
         'iss'=>'server',//签发人
         'exp'=>7200,//过期时间s
         'sub'=>'phpServer--socketServer',//主题
@@ -25,32 +27,40 @@ class JsonWebToken
     /**
      * jwt Header
      */
-    protected $jwtHeader = [
+    protected $Header = [
         'alg'=>'md5',
         'typ'=>'JWT',
     ];
+    /**
+     * @var mixed|string
+     */
+    protected $secret = 'sda564545464546464';
+
+    /**
+     * @var string
+     */
+    protected $token_name = 'token';
+
 
     /**
      * JsonWebToken constructor.
      *
      * @param array $Payload
-     * @param array $JWT_config
-     * @param null  $name
+     * @param null  $JWT_config
      */
-    function __construct($Payload=[],$JWT_config=[])
+    function __construct($Payload=[],$config=null)
     {
+        if(!$config) {return false;}
 
-        var_dump($JWT_config);
-        exit;
-        /**
-         * 处理配置
-         */
-        if($JWT_config != []){
-            $this->JWT_secret = $JWT_config['JWT_secret']??$this->JWT_secret;
-            $this->JWT_secret_base = $JWT_config['JWT_secret_base']??$this->JWT_secret_base;
-            $this->Access_token_name = $JWT_config['Access_token_name']??$this->Access_token_name;
-            $this->jwtHeader = isset($JWT_config['jwtHeader'])?array_merge($this->jwtHeader,$JWT_config['jwtHeader']):$this->jwtHeader;
+        if(is_array($config)){
+            $this->secret = $config['secret']??$this->secret;
+            $this->token_name = $config['token_name']??$this->token_name;
+            $this->Header = isset($config['Header'])?array_merge($this->jwtHeader,$config['jwtHeader']):$this->Header;
+        }else{
+            $this->init($JWT_config);
+
         }
+        exit;
         //$this->setJWT($Payload);
     }
     /**
@@ -82,6 +92,27 @@ class JsonWebToken
         $this->JWT_param  = '/?'.$this->Access_token_name.'='.$str;
 
     }
+
+    /**
+     * 获取配置
+     * @param $name
+     */
+    protected  function init($name)
+    {
+        $secretData = JsonWebTokenConfig::secret[$name];
+        /**
+         * Header
+         */
+        $this->Header = JsonWebTokenConfig::Header;
+        $this->Header['alg'] = $secretData['alg'];
+        /**
+         * secret
+         */
+        $this->secret = JsonWebTokenConfig::Payload[$secretData['Payload']];
+
+    }
+
+
     /**
      * 包括 加 解密
      *
